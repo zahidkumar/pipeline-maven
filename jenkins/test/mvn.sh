6924828eb5ee2fe22ef41c9a3dae4ad42c5f4872
EC2 Default User <ec2-user@ip-172-31-42-222.ap-south-1.compute.internal>
2021-02-09 12:04:09 +0000

    fi

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,6 +4,6 @@ echo "***************************"
 echo "** Tessting code**********"
 echo "***************************"
 
-WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/docker-pipeline
+WORKSPACE=/home/ec2-user/jenkins/jenkins_home/workspace/docker-pipeline
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
